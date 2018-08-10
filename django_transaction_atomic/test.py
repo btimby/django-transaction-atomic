7063c11295ff5aeb8da482c649dcf3cfc3b59078
Ben Timby <btimby@smartfile.com>
2018-08-10 11:52:51 -0400

    Add TestCase class to handle atomic.

---
+++ django_transaction_atomic/test.py
@@ -0,0 +1,27 @@
+
+from django.test import TestCase as BaseTestCase
+
+
+from . import atomic
+from ._atomic import atomic as _atomic
+
+
+if atomic == _atomic:
+
+    # If we are using our backported atomic decorator, then we must provide a
+    # backported TestCase to go with it.
+
+    class TestCase(BaseTestCase):
+        """
+        """
+
+        def _fixture_setup(self):
+            return super(TestCase, self)._fixture_setup()
+
+
+else:
+
+    # Otherwise, the builtin TestCase already handles atomic. We should provide
+    # it instead.
+
+    TestCase = BaseTestCase
